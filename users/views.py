6bf7dbfacbb2727c69eb856212d58d38f8e73c9e
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-01 12:05:45 +0300

    added views for addition page

--- users/views.py
+++ users/views.py
@@ -3,3 +3,43 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html',{})
+
+def add(request):
+    from random import randint
+
+    num_1 = randint(0,10)
+    num_2 = randint(0,10)
+
+    if request.method == "POST":
+        answer = request.POST.get('answer',False)
+        old_num_1 = request.POST.get('old_num_1',False)
+        old_num_2 = request.POST.get('old_num_2',False)
+
+        if not answer:
+            return render(request,'divide.html', {
+                'my_answer' : "You didn't entered any answer",
+                'num_1' : num_1,
+                'num_2' : num_2,
+                'color' : 'warning',
+                })
+
+        correct_answer = int(old_num_1) + int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct ! " + old_num_1 + " + " + old_num_2 + " = " + answer
+            color = "success"
+        else:
+            my_answer = "Incorrect ! " + old_num_1 + " + " + old_num_2 + " is  " + str(correct_answer) + ", not " + answer
+            color = "danger"
+
+        return render(request,'add.html', {
+            'answer':answer,
+            'my_answer':my_answer,
+            'num_1' : num_1,
+            'num_2' : num_2,
+            'color' : color,
+            })
+
+    return render(request,'add.html',{
+        'num_1' : num_1,
+        'num_2' : num_2,
+        })
