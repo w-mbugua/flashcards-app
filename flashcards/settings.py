a0dee0ac3baf857c99f83aa52f43439167db6aff
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-02 15:21:54 +0300

    update

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = 'os.path.join(BASE_DIR, "static")'
-STATIC_ROOT = "/flashcards_app/static/images/"
+STATIC_ROOT = "/static/images/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
