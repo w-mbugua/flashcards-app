fc5f323c4e1a7cc086e3a00595f412b64c1ce30f
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-06-03 12:13:16 +0300

    initial commit

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
 
 urlpatterns = [
@@ -8,12 +7,10 @@ urlpatterns = [
     path('subtract.html',views.subtract, name="subtract"),
     path('multiply.html',views.multiply, name="multiply"),
     path('divide.html',views.divide, name="divide"),
-=======
 
 from .views import home, createcards
 
 urlpatterns = [ 
     path('', home, name='home'),
     path('new/', createcards, name='new_card'),
->>>>>>> a36329b8d3981bc1dd868d27b838690c2ddbb514
 ]
